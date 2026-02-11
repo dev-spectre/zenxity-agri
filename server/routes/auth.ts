423bc8562d4d553e5321400642dbf44fe588d587
dev-spectre <abhishekdallasalpy@gmail.com>
2026-02-12 02:08:15 +0530

    fix import

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction, Router } from "express";
 import dotenv from "dotenv";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import nodemailer from "nodemailer";
 import { OAuth2Client } from "google-auth-library";
